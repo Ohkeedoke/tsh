bf59631fd33a83a683bb61fbec24815dfec055aa
Ohkeedoke <43252202+Ohkeedoke@users.noreply.github.com>
2020-07-19 22:01:23 +0200

    Task-3: New features (#3)
    * input-validation-added
    
    * dynamic-history-block-impl
    
    * toggle-class-while-requests-pending
    
    * RWD-changes
    
    * build-minified

--- src/app.js
+++ src/app.js
@@ -1,19 +1,87 @@
 import './assets/scss/app.scss';
 import $ from 'cash-dom';
+import { getTemplate } from './htmlTemplates/templatesGenerator';
 
+const inputDOM = $('.username.input');
+const loadButtonDOM = $('.load-username');
+const userTimelineDOM = document.getElementById('user-timeline');
+const loaderDOM = $('#spinner');
+const profileDOM = $('.profile');
 
 export class App {
   initializeApp() {
-    $('.load-username').on('click', () => {
-      let userName = $('.username.input').val();
+    loadButtonDOM.on('click', () => {
 
-      fetch('https://api.github.com/users/' + userName)
-        .then(response => response.json())
-        .then(body => {
-          this.profile = body;
-          this.update_profile();
-        })
+      if (this.validation(inputDOM.val())) {
+        this.fetchData();
+      } else {
+        inputDOM.addClass('input--invalid');
+      }
+
     })
+
+    inputDOM.on('change', () => {
+      if (inputDOM.hasClass('input--invalid')) {
+        inputDOM.removeClass('input--invalid');
+      }
+    });
+  }
+
+  fetchData() {
+    this.toggleVisibility('pending')
+    fetch('https://api.github.com/users/' + inputDOM.val())
+      .then(response => response.json())
+      .then(body => {
+        this.profile = body;
+        this.update_profile();
+        this.fetchHistory();
+        this.toggleVisibility('completed')
+      })
+  }
+
+  fetchHistory() {
+    this.toggleVisibility('pending')
+    fetch(`https://api.github.com/users/${inputDOM.val()}/events/public`)
+    .then(response => response.json())
+    .then(body => {
+      this.history = body;
+      this.appendHistory();
+      this.toggleVisibility('completed')
+    })
+  }
+
+  toggleVisibility(status) {
+    switch (status) {
+      case 'pending':
+        loaderDOM.removeClass('is-hidden');
+        profileDOM.addClass('is-hidden');
+        userTimelineDOM.classList.add('is-hidden');
+        break;
+      case 'completed':
+        loaderDOM.addClass('is-hidden');
+        profileDOM.removeClass('is-hidden');
+        userTimelineDOM.classList.remove('is-hidden');
+        break;
+    }
+  }
+
+  appendHistory() {
+    let filteredBody = this.history.filter(el => {
+      return el.type === 'PullRequestEvent' || el.type === 'PullRequestReviewCommentEvent';
+    })
+
+    let htmlHistory = ''
+
+    for (let i = 0; i < filteredBody.length; i++) {
+      htmlHistory += getTemplate(filteredBody[i]);
+    }
+
+    userTimelineDOM.innerHTML = htmlHistory;
+  }
+
+  validation(text) {
+    const pattern = new RegExp(/^(\d|[a-z]|[\w_]|[\w-])+$/);
+    return pattern.test(text);
   }
 
   update_profile() {
